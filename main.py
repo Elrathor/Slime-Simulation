8749df77fd18947e9cfc4fabc8a6ae0593bc8854
Tobias Boyer <tobias.boyer@outlook.com>
2021-11-10 22:47:43 +0100

    Introduced Multicore Processing
    Burn CPU Burn!

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
+import multiprocessing
 import random
+import time
 
 import numpy as np
 from scipy import ndimage
 from PIL import Image as im
+from functools import partial
 
-height = 128
-width = 128
-number_of_agents = 200
+height = 1080
+width = 1920
+number_of_agents = 20000
 step_size = 2
-simulation_length = 10000
+simulation_length = 20
+simulation_report_step_size = 10
 mutation_probability = 0.6
+multiprocessing_pool_size = 8
 
 
 class Agent:
@@ -81,19 +86,46 @@ def draw_agent(agent, image):
     return image
 
 
+def move_agent_helper(image, agent):
+    agent.move(image)
+    return agent
+
+
 def main():
     agents = []
     for agent_number in range(number_of_agents):
         agents.append(Agent(random.randrange(0, height - 1), random.randrange(0, width - 1)))
-
     raw_image_array = np.full((height, width), 0, np.uint8)
 
+    average_time_processing = 0
+    average_time_drawing = 0
     for i in range(simulation_length):
-        for agent in agents:
-            agent.move(raw_image_array)
-            raw_image_array = draw_agent(agent, raw_image_array)
-        raw_image_array = ndimage.gaussian_filter(raw_image_array, sigma=0.5)
+        start_time = time.time()
+        mp_pool_partial = partial(move_agent_helper, raw_image_array)
+        mp_pool = multiprocessing.Pool(multiprocessing_pool_size)
+        result = mp_pool.map(mp_pool_partial, agents)
+        mp_pool.close()
+        mp_pool.join()
+        agents = result
+        end_time = time.time()
+        average_time_processing += (end_time-start_time)
 
+        start_time = time.time()
+        for agent in agents:
+            raw_image_array = draw_agent(agent, raw_image_array)
+
+        #raw_image_array = ndimage.gaussian_filter(raw_image_array, sigma=0.25) # slower but fancy
+        raw_image_array = raw_image_array // 2 # faster but less fancy and more dangerous
+        end_time = time.time()
+        average_time_drawing += (end_time - start_time)
+        
+        if (i + 1) % simulation_report_step_size == 0:
+            print("Simulation Progress (length):", i)
+            print("Simulation Prozessing Average (seconds):", average_time_processing / simulation_report_step_size)
+            print("Simulation Drawing Average (seconds):", average_time_drawing / simulation_report_step_size)
+            print()
+            average_time_processing = 0
+            average_time_drawing = 0
 
     # creating image object of above array
     data = im.fromarray(raw_image_array)
